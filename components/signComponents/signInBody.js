ae26b47732951fe12cc2653b1dbac86966335300
Muklah Hilal Naji <M-M.mexat@hotmail.com>
2019-01-17 02:40:19 +0300

    really last commit

--- components/signComponents/signInBody.js
+++ components/signComponents/signInBody.js
@@ -98,7 +98,7 @@ export default class SignInBody extends Component {
         </View>
         <Button
           title="continue"
-          color="#ffffff"
+          color="#000"
           onPress={() => {
             const toFirebase = {
               email: this.state.email,
