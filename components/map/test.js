7fa949eb97fff8b89df6056241597f548a6ec7a3
Ramy Majid <rany.ramy2009@gmail.com>
2019-02-12 23:43:34 +0300

    removed test .js

--- components/map/test.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-
-export default class Test extends Component {
-  render() {
-    return <Text>hello world</Text>;
-  }
-}
